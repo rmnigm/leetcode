3bc284793d9640d766bd0fa86d41a789ebe285b6
Roman Nigmatullin <rmnigmatullin>
2023-03-10 18:26:28 +0300

    first task on python, merge k sorted lists

---
+++ python/023.py
@@ -0,0 +1,28 @@
+import heapq
+from collections import deque
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+        new_head = ListNode()
+        current_element = new_head
+        pointers = {i: node for i, node in enumerate(lists) if node is not None}
+        if pointers:
+            for i, q in pointers.items():
+                heap.append((q.val, i))
+                pointers[i] = q.next
+            heapq.heapify(heap)
+            while heap:
+                value, i = heapq.heappop(heap)
+                current_element.next = ListNode(value)
+                current_element = current_element.next
+                if pointers[i]:
+                    heapq.heappush(heap, (pointers[i].val, i))
+                    pointers[i] = pointers[i].next
+        return new_head.next
