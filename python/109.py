e36f2d5d37ede6d2c7bc72a6b1ef3be39acd39b4
Roman Nigmatullin <rmnigmatullin>
2023-03-11 16:47:18 +0300

    added 109 form bst from linked sorted list

---
+++ python/109.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        sorted_array = []
+        while head:
+            sorted_array.append(head.val)
+            head = head.next
+        
+        def buildBST(l, r):
+            if l > r:
+                return None
+            mid = (l + r) // 2
+            node = TreeNode(sorted_array[mid])
+            if l == r:
+                return node
+            node.left = buildBST(l, mid - 1)
+            node.right = buildBST(mid + 1, r)
+            return node
+        
+        return buildBST(0, len(sorted_array) - 1)
